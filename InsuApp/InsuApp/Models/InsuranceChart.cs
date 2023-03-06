896af59d4a030ae54210da7081d30f3121ea6ac5
luke0926 <lukas.navry@gmail.com>
2023-03-06 15:26:25 +0100

    InsuChart_update

---
+++ InsuApp/InsuApp/Models/InsuranceChart.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+
+namespace InsuApp1.Models
+{
+	public class InsuranceChart
+	{
+		public virtual IEnumerable<UserInsurance>? InsuranceValues { get; set; }
+		public string? InsuranceName { get; set; }
+		public int? InsuranceTotalValue { get { return InsuranceValues.Sum(x => x.InsuranceValue); } }
+	}
+}
